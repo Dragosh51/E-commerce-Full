3a54b78285c086db4a7456ff80b28049346d5e42
Dragosh51 <dragosh.bindila@gmail.com>
2024-03-21 10:57:00 +0000

    cart style

--- frontend/src/components/cart/Cart.js
+++ frontend/src/components/cart/Cart.js
@@ -1,3 +1,4 @@
+import './Cart.css';
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { clearCart, removeItem } from '../../store/actions/cartActions';
@@ -15,23 +16,28 @@ const Cart = () => {
   };
 
   return (
-    <div>
-      <h2>Your Cart</h2>
-      {cartItems.length === 0 ? (
-        <p>Your cart is empty.</p>
-      ) : (
-        <div>
-          <button onClick={handleClearCart}>Clear Cart</button>
-          <ul>
-            {cartItems.map((item) => (
-              <li key={item.productID}>
-                {item.productName} - Quantity: {item.quantity ? item.quantity : 0}
-                <button onClick={() => handleRemoveItem(item.productID)}>Remove</button>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+    <div className="cart_component">
+      <div className="cart_container">
+        <h2>Your Cart</h2>
+        {cartItems.length === 0 ? (
+          <p>Your cart is empty.</p>
+        ) : (
+          <div>
+            <button onClick={handleClearCart}>Clear Cart</button>
+            <ul>
+              {cartItems.map((item) => (
+                <li key={item.productID}>
+                  {item.productName} - Quantity: {item.quantity ? item.quantity : 0}
+                  <button onClick={() => handleRemoveItem(item.productID)}>Remove</button>
+                </li>
+              ))}
+            </ul>
+            <div className="checkout-button-container">
+              <button className="checkout-button">Checkout</button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
